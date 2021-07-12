768a95b9f1ecd0bb4119e01557e038a464bba9b1
HADES <1005710179@qq.com>
2021-07-12 17:31:02 +0800

    请求封装

---
+++ src/utils/http/index.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import { getToken } from '@/utils/auth'
+axios.defaults.headers['Content-Type'] = 'application/json;charset=utf-8'
+
+// 创建axios实例
+const service = axios.create({
+    // axios中请求配置有baseURL选项，表示请求URL公共部分
+    baseURL: process.env.VUE_APP_BASE_API,
+    // 超时
+    timeout: 10000
+})
+// request拦截器
+service.interceptors.request.use(config => {
+     // 是否需要设置 token
+    const isToken = (config.headers || {}).isToken === false
+    if (getToken() && !isToken) {
+        config.headers['awesome'] =  getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
+    }
+    return config
+}, error => {
+    console.log(error)
+    Promise.reject(error)
+})
+
+// 响应拦截器
+service.interceptors.response.use(response =>{
+    
+})
